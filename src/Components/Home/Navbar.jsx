1c027de44f684551a5902650aad3b9b2d9d0a98e
Ibrahim-Ahamed <iamibruh@gmail.com>
2025-01-22 22:38:28 +0600

    modyfying the navbar

--- src/Components/Home/Navbar.jsx
+++ src/Components/Home/Navbar.jsx
@@ -1,17 +1,30 @@
 import { Link } from 'react-router'
 import { useAuth } from '../Context/AuthContext'
+import { useState } from 'react'
 
 const Navbar = () => {
   const { user, logOut } = useAuth()
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   const handleLogOut = () => {
     logOut()
   }
-  console.log(user)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
-    <div className="navbar max-w-screen-2xl container mx-auto px-5 bg-white mb-8 mt-2">
+    <div className="navbar max-w-screen-2xl container mx-auto px-5 bg-white mb-8 mt-2 relative">
       <div className="navbar-start">
+        {/* Hamburger Menu */}
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <button
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost lg:hidden"
+            onClick={toggleMenu}
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -26,74 +39,66 @@ const Navbar = () => {
                 d="M4 6h16M4 12h8m-8 6h16"
               />
             </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[2] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <Link to="/"> Home</Link>
-            </li>
-            <li>
-              <Link to="/services">Services</Link>
-            </li>
-            <li>
-              <Link to="/blogs">Blogs</Link>
-            </li>
-            <li>
-              <a>Catagorey</a>
-              <ul className="p-2">
-                <li>
-                  <Link to="">Web Development</Link>
-                </li>
-                <li>
-                  <Link to="">Block Chain with AI</Link>
-                </li>
-                <li>
-                  <Link to="">Dev OPPS</Link>
-                </li>
-              </ul>
-            </li>
-          </ul>
+          </button>
         </div>
-        <Link to="/" className="text-xl font-semibold  flex items-center">
+
+        {/* Logo */}
+        <Link
+          to="/"
+          className="text-xl font-semibold flex items-center justify-center"
+        >
           <img src="/logo.png" alt="" />
-          <span className="text-[#697077] ml-2 hidden sm:block"> WebCode</span>
+          <span className="text-[#697077] ml-2 hidden sm:block">WebCode</span>
         </Link>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
+
+      {/* Navbar Links (Large Screens) */}
+      <div className="navbar-center hidden lg:flex justify-center">
+        <ul className="menu menu-horizontal px-1 text-center">
           <li>
-            <Link to="/"> Home</Link>
+            <Link to="/" className="hover:text-gray-300">
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/services">Services</Link>
+            <Link to="/services" className="hover:text-gray-300">
+              Services
+            </Link>
           </li>
           <li>
-            <Link to="/blogs">Blogs</Link>
+            <Link to="/blogs" className="hover:text-gray-300">
+              Blogs
+            </Link>
           </li>
           <li>
             <details className="z-10">
-              <summary>Catagorey</summary>
+              <summary className="hover:text-gray-300">Category</summary>
               <ul className="p-2">
                 <li>
-                  <Link to="">Web Development</Link>
+                  <Link to="" className="hover:bg-gray-700 hover:text-gray-300">
+                    Web Development
+                  </Link>
                 </li>
                 <li>
-                  <Link to="">Block Chain with AI</Link>
+                  <Link to="" className="hover:bg-gray-700 hover:text-gray-300">
+                    Blockchain with AI
+                  </Link>
                 </li>
                 <li>
-                  <Link to="">Dev OPPS</Link>
+                  <Link to="" className="hover:bg-gray-700 hover:text-gray-300">
+                    DevOps
+                  </Link>
                 </li>
               </ul>
             </details>
           </li>
         </ul>
       </div>
+
+      {/* User Authentication */}
       <div className="navbar-end gap-4">
         {user ? (
           <>
-            {' '}
             <Link
               to="/Dashboard"
               className="btn btn-outline btn-primary px-8 hidden sm:flex"
@@ -102,14 +107,13 @@ const Navbar = () => {
             </Link>
             <button
               onClick={handleLogOut}
-              className="btn  btn-primary  hover:bg-primary px-8"
+              className="btn btn-primary hover:bg-primary px-8"
             >
               Logout
             </button>
           </>
         ) : (
           <>
-            {' '}
             <Link
               to="/login"
               className="btn btn-outline btn-primary px-8 hidden sm:flex"
@@ -118,12 +122,85 @@ const Navbar = () => {
             </Link>
             <Link
               to="/pricing"
-              className="btn  btn-primary  hover:bg-primary px-8"
+              className="btn btn-primary hover:bg-primary px-8"
             >
               Start Free Trial
             </Link>
           </>
         )}
+      </div>
+
+      {/* Navbar Links (Small Screens) */}
+      <div
+        className={`absolute top-0 left-0 w-full z-20 transform transition-transform duration-300 ease-in-out ${
+          isMenuOpen
+            ? 'translate-y-0 bg-gray-900'
+            : '-translate-y-full bg-white'
+        }`}
+      >
+        <ul className="menu menu-vertical p-4 text-center text-white">
+          <li>
+            <Link
+              to="/"
+              className="hover:bg-gray-700 hover:text-gray-300"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/services"
+              className="hover:bg-gray-700 hover:text-gray-300"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/blogs"
+              className="hover:bg-gray-700 hover:text-gray-300"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Blogs
+            </Link>
+          </li>
+          <li>
+            <details>
+              <summary className="hover:text-gray-300">Category</summary>
+              <ul className="p-2">
+                <li>
+                  <Link
+                    to=""
+                    className="hover:bg-gray-700 hover:text-gray-300"
+                    onClick={() => setIsMenuOpen(false)}
+                  >
+                    Web Development
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to=""
+                    className="hover:bg-gray-700 hover:text-gray-300"
+                    onClick={() => setIsMenuOpen(false)}
+                  >
+                    Blockchain with AI
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to=""
+                    className="hover:bg-gray-700 hover:text-gray-300"
+                    onClick={() => setIsMenuOpen(false)}
+                  >
+                    DevOps
+                  </Link>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </ul>
       </div>
     </div>
   )
